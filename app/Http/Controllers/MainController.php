6e2b28a00aea86dfaa804b00ef586ee2a40489a4
Federico Lufrano <105553021+lufranski@users.noreply.github.com>
2023-02-06 15:44:00 +0100

    Displaying every person data

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,11 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Person;
+
 class MainController extends Controller
 {
     
     public function home(){
 
-        return view('pages.home');
+        // metodo compatto con compact
+        $people = Person::all();
+
+        // metodo esteso con array data, in questo caso bisognerebbe agganciare $data al posto di compact['nomeArray']
+        // $data = [
+
+        //     'people' => $people
+        // ]
+
+        return view('pages.home', compact('people'));
     }
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,6 +2,21 @@
 
 @section('content')
 
-    <h1>CONTENT</h1>
+    <h1>People</h1>
+
+    <ul>
+        @foreach ($people as $person)
+            
+            <li>
+                <h6>
+
+                    {{ $person -> firstName }} {{ $person -> lastName }} 
+                </h6>
+                <p>
+                    Born in: {{ $person -> dateOfBirth }}, Height: {{ $person -> height }} cm
+                </p>
+            </li>
+        @endforeach
+    </ul>
     
 @endsection
